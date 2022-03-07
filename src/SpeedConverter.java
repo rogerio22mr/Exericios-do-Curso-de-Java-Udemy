4bd665d13ffbeed48502ac795155f36c1f180ac4
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2022-03-07 07:51:23 -0300

    Speed Converter

---
+++ src/SpeedConverter.java
@@ -0,0 +1,26 @@
+public class SpeedConverter {
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        double milesPerHour = kilometersPerHour / 1.609;
+        return Math.round(milesPerHour);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
