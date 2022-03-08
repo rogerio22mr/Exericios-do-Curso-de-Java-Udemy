60e9fa6b73cd3411591c654419d90f2f8422724f
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2022-03-07 23:01:19 -0300

    Equal Sum Checker

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,13 @@
+public class EqualSumChecker {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasEqualSum(int n1, int n2, int n3) {
+        if (n1 + n2 == n3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
